bfc3c15d86edf882fad9fa470ec40563a3ce276a
Gloria <825428390@qq.com>
2017-06-17 23:32:36 +0800

    Excellent git status! you can do somthing in a room.

--- src/wyykt/gloria/practice/castle/Game.java
+++ src/wyykt/gloria/practice/castle/Game.java
@@ -37,8 +37,18 @@ public class Game {
 		
 		@Override
 		public void doCmd(String word) {
-			System.out.println("迷路了吗？你可以做的命令有：go bye help");
+			System.out.println("迷路了吗？你可以做的命令有：go do bye help");
 			System.out.println("如：\tgo east");
+			System.out.println("\tdo game");
+		}
+		
+	}
+	
+	private class HandlerDo extends Handler {
+		
+		@Override
+		public void doCmd(String word) {
+			doSomething(word);
 		}
 		
 	}
@@ -51,19 +61,42 @@ public class Game {
 		handlers.put("go", new HandlerGo());
 		handlers.put("bye", new HandlerBye());
 		handlers.put("help", new HandlerHelp());
+		handlers.put("do", new HandlerDo());
 		createRooms();
 	}
 
 	private void createRooms() {
 		Room outside, lobby, pub, study, bedroom, exercise;
+		
+		// 可以干的事
+		Specialty drink = new Specialty("drink", "喝水", "嗯~~ 好喝！！");
+		Specialty beer = new Specialty("beer", "喝啤酒", "来来来，干杯！");
+		Specialty book = new Specialty("book", "看书", "这本书不错，带回家好好看。");
+		Specialty bed = new Specialty("bed", "睡觉" ,"睡了一会觉，精力很充沛。");
+		Specialty ecise = new Specialty("exercise", "跑步", "速度七十迈，心情是自由自在，希望终点是爱琴海，全力奔跑梦在彼岸");
+		Specialty game = new Specialty("game", "玩手游", "哈哈，又升了一级，哦耶！！");
 
 		// 制造房间
 		outside = new Room("城堡外");
+		outside.setSomth(game);
+		outside.setSomth(ecise);
 		lobby = new Room("大堂");
+		lobby.setSomth(book);
 		pub = new Room("小酒吧");
+		pub.setSomth(beer);
+		pub.setSomth(bed);
 		study = new Room("书房");
+		study.setSomth(book);
+		study.setSomth(drink);
+		study.setSomth(game);
 		bedroom = new Room("卧室");
+		bedroom.setSomth(bed);
+		bedroom.setSomth(beer);
+		bedroom.setSomth(game);
 		exercise = new Room("健身房");
+		exercise.setSomth(ecise);
+		exercise.setSomth(drink);
+		exercise.setSomth(game);
 
 		// 初始化房间的出口
 		outside.setExit("east", lobby);
@@ -94,7 +127,27 @@ public class Game {
 		System.out.println("你在" + currentRoom);
 		System.out.print("出口有：");
 		System.out.print(currentRoom.getExitDesc());
+		
+		if(currentRoom.hasSomethingToDo()){
+			System.out.print("  你可以：");
+			System.out.print(currentRoom.getSomthdoDesc());
+		}
+		
 		System.out.println();
+	}
+	
+	
+	
+	public void doSomething(String what){
+		Specialty s = currentRoom.getSpecialty(what);
+		
+		if(s == null){
+			System.out.println("在这里做不了");
+		} else {
+			System.out.println(s.getEffect());
+		}
+		
+		
 	}
 
 
@@ -108,6 +161,7 @@ public class Game {
 			printPrompt();
 		}
 	}
+
 
 	public void play() {
 		Scanner in = new Scanner(System.in);
--- src/wyykt/gloria/practice/castle/Room.java
+++ src/wyykt/gloria/practice/castle/Room.java
@@ -1,26 +1,27 @@
 package wyykt.gloria.practice.castle;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Room {
     private String description;
     private HashMap<String, Room> exits = new HashMap<String, Room>();
+    private ArrayList<Specialty> somthdo = new ArrayList<Specialty>();
 
     public Room(String description) 
     {
         this.description = description;
     }
-
+   
     public void setExit(String dir, Room room){
     	exits.put(dir,  room);
     }
     
-
-    @Override
-    public String toString()
-    {
-        return description;
+    public void setSomth(Specialty s){
+    	somthdo.add(s);
     }
+    
+
     
     public String getExitDesc(){
     	StringBuffer sb = new StringBuffer();
@@ -32,8 +33,40 @@ public class Room {
     	return sb.toString();
     }
     
+    public boolean hasSomethingToDo(){
+    	return somthdo.size() > 0;
+    }
+    
+    public String getSomthdoDesc(){
+    	StringBuffer sb = new StringBuffer();
+    	for(Specialty smth : somthdo){
+    		sb.append(smth.getWhat());
+    		sb.append("("+smth.getDesc()+")");
+    		sb.append(" ");
+    	}
+
+    	return sb.toString();
+    }
+    
+    
     public Room getExit(String direction){
     	return exits.get(direction);
+    }
+    
+    public Specialty getSpecialty(String what){
+    	for (Specialty s : somthdo){
+    		if(s.getWhat().equals(what)){
+    			return s;
+    		}
+    	}
+    	return null;
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        return description;
     }
     
     
